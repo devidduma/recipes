c814e3a285bb32ee450b91b6250ad05e060cfef1
devidduma <devidd05@gmail.com>
2025-05-27 12:08:23 +0200

    build: Database credentials provided in settings

--- recipes/settings.py
+++ recipes/settings.py
@@ -77,10 +77,11 @@ WSGI_APPLICATION = 'recipes.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS')
+        'HOST': 'localhost',
+        'NAME': 'recipes',
+        'USER': 'postgres',
+        'PASSWORD': 'pass',
+        'PORT': '5432'
     }
 }
 
