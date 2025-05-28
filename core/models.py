64f7e8e31b4f4990002031a760d823648539e99a
devidduma <devidd05@gmail.com>
2025-05-28 11:25:23 +0200

    feat(core): Develop create superuser in user manager

--- core/models.py
+++ core/models.py
@@ -11,15 +11,25 @@ PermissionsMixin
 class UserManager(BaseUserManager):
     """ Manager for Users. """
 
-    def create_user(self, email, password=None, **extra_field):
+    def create_user(self, email, password=None, **extra_fields):
         """ Create, save and return a new User. """
         if not email:
             raise ValueError('User must have an email address.')
-        user = self.model(email=self.normalize_email(email), **extra_field)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """ Create and return a new SuperUser. """
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """ User in the system. """
