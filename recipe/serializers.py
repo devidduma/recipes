b5e4cff33cee0bff13fb1339a80e02de01ef1490
devidduma <devidd05@gmail.com>
2025-06-18 17:46:26 +0200

    feat(recipe): Update recipe

--- recipe/serializers.py
+++ recipe/serializers.py
@@ -43,6 +43,19 @@ class RecipeSerializer(serializers.ModelSerializer):
 
         return recipe
 
+    def update(self, instance, validated_data):
+        """ Update a recipe. """
+        tags = validated_data.pop('tags', None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
+
 class RecipeDetailSerializer(RecipeSerializer):
     """ Serializer for recipe detail view. """
 
