473a01d6cc5ec646fe30dfa7c6f0ea842baef346
devidduma <devidd05@gmail.com>
2025-06-19 14:58:07 +0200

    fix(recipe): Fix get user model

--- recipe/tests/test_ingredients_api.py
+++ recipe/tests/test_ingredients_api.py
@@ -15,7 +15,7 @@ INGREDIENTS_URL = reverse('recipe:ingredient-list')
 def create_user(email='user@example.com', password='testpass123'):
     """ Create and return user. """
 
-    return get_user_model.objects.create_user(email=email, password=password)
+    return get_user_model().objects.create_user(email=email, password=password)
 
 class PublicIngredientsApiTests(TestCase):
     """ Test unauthenticated API requests. """
