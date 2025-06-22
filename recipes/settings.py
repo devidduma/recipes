edc9c172eed25b092ca212248ae2a82848158e7d
devidduma <devidd05@gmail.com>
2025-06-22 17:07:14 +0200

    build: Spectacular settings

--- recipes/settings.py
+++ recipes/settings.py
@@ -145,3 +145,9 @@ AUTH_USER_MODEL = 'core.User'
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
 }
+
+# Spectacular Settings
+
+SPECTACULAR_SETTINGS = {
+    'COMPONENT_SPLIT_REQUEST': True
+}
